d85af90bdbbcc5076438653d117fe29fa73fdf0e
m1tsuyaaa <stepan.katkovxx@gmail.com>
2025-09-29 23:51:58 +0700

    lab1 Update2
    29.03.25

---
+++ lab1.cpp
@@ -0,0 +1,45 @@
+/*************************
+ * Name:    Katkov Stepan*
+ * Project: Lab01        *
+ * Var 8                 *
+ * ***********************/ 
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+
+int main() {
+
+    double lambda, R1, R2, t1, t2, F, L;
+
+    cout << "Enter lambda =";
+    cin  >> lambda;
+    cout << "Enter R1 =";
+    cin  >> R1;
+    cout << "Enter R2 =";
+    cin  >> R2;
+    cout << "Enter t1 =";
+    cin  >> t1;
+    cout << "Enter t2 =";
+    cin  >> t2;
+    cout << "Enter F =";
+    cin  >> F;
+    cout << "Enter L =";
+    cin  >> L;
+
+    double deltaT, Q1, Q2, Q3;
+    deltaT = t1 - t2;
+    
+    Q1 = (lambda * F / (R2 - R1)) * deltaT;
+    
+    Q2 = (2 * PI * lambda * L / log(R2 / R1)) * deltaT;
+
+    Q3 = (4 * PI * lambda / (1/R1 - 1/R2)) * deltaT;
+
+    cout << Q1 << endl << Q2 << endl << Q3 << endl;
+
+    return 0;
+}
